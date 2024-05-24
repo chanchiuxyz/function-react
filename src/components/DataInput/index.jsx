3cfce7520b41a87a0d6a7fb0dd5c400bbe56552b
chanchiuxyz <chanchiuxyz@gmail.com>
2024-05-23 18:14:21 -0700

    use createRef() to get the data of <input>tag

--- src/components/DataInput/index.jsx
+++ src/components/DataInput/index.jsx
@@ -3,11 +3,17 @@ import PubSub from 'pubsub-js'
 import './index.css'
 
 export default class DataInput extends Component {
+
+    nameRef = React.createRef()
+    titleRef = React.createRef()
+    mobileRef = React.createRef()
+
     publishMsg = () => {
-        const dataInputs = document.getElementsByTagName('input')
-        const dataObject = {name: dataInputs[0].value,
-                           title: dataInputs[1].value,
-                           mobile: dataInputs[2].value.replace(/(\d{3})(\d{3})(\d+)/g, "($1) $2-$3")
+        // const dataInputs = document.getElementsByTagName('input')
+        // console.log(this.nameRef)
+        const dataObject = {name: this.nameRef.current.value,
+                           title: this.titleRef.current.value,
+                           mobile: this.mobileRef.current.value.replace(/(\d{3})(\d{3})(\d+)/g, "($1) $2-$3")
                            }
             
         PubSub.publish('dataInput',dataObject)
@@ -19,11 +25,11 @@ export default class DataInput extends Component {
         <div className="dataInput">
              
                  
-            <input type="text" id="name" placeholder="Enter your name" />
+            <input type="text" ref={this.nameRef} id="name" placeholder="Enter your name" />
 
-            <input type="text" className="title" id="title" placeholder="Enter your title" />
+            <input type="text" ref={this.titleRef} className="title" id="title" placeholder="Enter your title" />
 
-            <input type="text" className="mobile" id="mobile" placeholder="Enter your phone number" />
+            <input type="text" ref={this.mobileRef} className="mobile" id="mobile" placeholder="Enter your phone number" />
                 
             <button id="dataSubmit" onClick={this.publishMsg}>Submit</button>
 
